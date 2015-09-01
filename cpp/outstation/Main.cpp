2943dadb244806b8d742f0f464cd03f3cd6e9bd9
Ben Grindy <ben.grindy@windlogics.com>
2015-09-01 10:35:53 -0500

    BAT-44 - convert from demo outstation to new outstation app, add TODOs for implementation

--- cpp/outstation/Main.cpp
+++ cpp/outstation/Main.cpp
@@ -1,10 +1,7 @@
 /**
- * Licensed to Green Energy Corp (www.greenenergycorp.com) under one or
- * more contributor license agreements. See the NOTICE file distributed
- * with this work for additional information regarding copyright ownership.
- * Green Energy Corp licenses this file to you under the Apache License,
- * Version 2.0 (the "License"); you may not use this file except in
- * compliance with the License.  You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -13,10 +10,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- * This project was forked on 01/01/2013 by Automatak, LLC and modifications
- * may have been made to this file. Automatak, LLC licenses these modifications
- * to you under the terms of the License.
  */
 
 #include <asiodnp3/DNP3Manager.h>
@@ -32,6 +25,8 @@
 #include <string>
 #include <thread>
 #include <iostream>
+#include <csignal>
+#include <unistd.h>
 
 #include "JSONCommandHandler.h"
 
@@ -41,120 +36,54 @@ using namespace openpal;
 using namespace asiopal;
 using namespace asiodnp3;
 
+namespace {
+volatile std::sig_atomic_t gSignalStatus;
+}
+
+void signal_handler(int signal) {
+	gSignalStatus = signal;
+}
+
 void ConfigureDatabase(DatabaseConfigView view) {
-	// example of configuring analog index 0 for Class2 with floating point variations by default
+	// TODO read from config file
 	view.analogs[0].variation = StaticAnalogVariation::Group30Var5;
 	view.analogs[0].metadata.clazz = PointClass::Class2;
 	view.analogs[0].metadata.variation = EventAnalogVariation::Group32Var7;
 }
 
 int main(int argc, char* argv[]) {
-	// Specify what log levels to use. NORMAL is warning and above
-	// You can add all the comms logging by uncommenting below.
-	const uint32_t FILTERS = levels::NORMAL; // | levels::ALL_COMMS;
+	const uint32_t FILTERS = levels::NORMAL;
 
-	// This is the main point of interaction with the stack
-	// Allocate a single thread to the pool since this is a single outstation
 	DNP3Manager manager(1);
-
-	// send log messages to the console
 	manager.AddLogSubscriber(&ConsoleLogger::Instance());
 
-	// Create a TCP server (listener)	
-	auto pChannel = manager.AddTCPServer("server", FILTERS,
-			TimeDuration::Seconds(5), TimeDuration::Seconds(5), "0.0.0.0",
-			20000);
-
-	// Optionally, you can bind listeners to the channel to get state change notifications
-	// This listener just prints the changes to the console
+	auto pChannel = manager.AddTCPServer("server", FILTERS, TimeDuration::Seconds(5), TimeDuration::Seconds(5), "0.0.0.0", 20000);
 	pChannel->AddStateListener([](ChannelState state)
 	{
 		std::cout << "channel state: " << ChannelStateToString(state) << std::endl;
 	});
 
-	// The main object for a outstation. The defaults are useable, 
-	// but understanding the options are important.
 	OutstationStackConfig stackConfig;
-
-	// You must specify the shape of your database and the size of the event buffers
 	stackConfig.dbTemplate = DatabaseTemplate::AllTypes(10);
 	stackConfig.outstation.eventBufferConfig = EventBufferConfig::AllTypes(10);
-
-	// you can override an default outstation parameters here
-	// in this example, we've enabled the oustation to use unsolicted reporting
-	// if the master enables it
-	stackConfig.outstation.params.allowUnsolicited = true;
-
-	// You can override the default link layer settings here
-	// in this example we've changed the default link layer addressing
 	stackConfig.link.LocalAddr = 10;
 	stackConfig.link.RemoteAddr = 1;
 
-	// Create a new outstation with a log level, command handler, and
-	// config info this	returns a thread-safe interface used for
-	// updating the outstation's database.
-	auto pOutstation = pChannel->AddOutstation("outstation",
-			JSONCommandHandler::Instance(),
-			DefaultOutstationApplication::Instance(), stackConfig);
+	// TODO configure and run (tcp or other) server for JSON commands/events
+	// 1. convert incoming JSON to MeasUpdate and apply to outstation instance
+	// 2. JSONCommandHandler incoming DNP3 commands should convert and forward via JSON
 
-	// You can optionally change the default reporting variations or class assignment prior to enabling the outstation
+	auto pOutstation = pChannel->AddOutstation("outstation", JSONCommandHandler::Instance(), DefaultOutstationApplication::Instance(), stackConfig);
+
+	// Configure and start outstation
 	ConfigureDatabase(pOutstation->GetConfigView());
-
-	// Enable the outstation and start communications
 	pOutstation->Enable();
 
-	// variables used in example loop
-	string input;
-	uint32_t count = 0;
-	double value = 0;
-	bool binary = false;
-	DoubleBit dbit = DoubleBit::DETERMINED_OFF;
+	// Initiate shutdown when signal received
+	std::signal(SIGINT, signal_handler);
+	pause();
 
-	while (true) {
-		std::cout << "Enter one or more measurement changes then press <enter>"
-				<< std::endl;
-		std::cout
-				<< "c = counter, b = binary, d = doublebit, a = analog, x = exit"
-				<< std::endl;
-		std::cin >> input;
-
-		MeasUpdate tx(pOutstation);
-
-		for (char& c : input) {
-			switch (c) {
-			case ('c'): {
-				tx.Update(Counter(count), 0);
-				++count;
-				break;
-			}
-			case ('a'): {
-				tx.Update(Analog(value), 0);
-				value += 1;
-				break;
-			}
-			case ('b'): {
-				tx.Update(Binary(binary), 0);
-				binary = !binary;
-				break;
-			}
-			case ('d'): {
-				tx.Update(DoubleBitBinary(dbit), 0);
-				dbit = (dbit == DoubleBit::DETERMINED_OFF) ?
-						DoubleBit::DETERMINED_ON : DoubleBit::DETERMINED_OFF;
-				break;
-			}
-			case ('x'):
-
-				// DNP3Manager destructor cleanups up everything automagically
-				return 0;
-
-			default:
-				std::cout << "No action registered for: " << c << std::endl;
-				break;
-			}
-		}
-
-	}
-
+	pOutstation->Shutdown();
+	manager.Shutdown();
 	return 0;
 }
