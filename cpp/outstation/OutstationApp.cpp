87920eb5a4fc427aa206bd1089306cf83958df18
Ben Grindy <ben.grindy@windlogics.com>
2015-10-08 15:20:11 -0500

    BAT-45
    - add program_options for outstation app usage
    - first pass at yaml outstation config support, currently only used to
    determine number of analogs,counters, etc.
    - added local/remote outstation address yaml conf options

--- cpp/outstation/OutstationApp.cpp
+++ cpp/outstation/OutstationApp.cpp
@@ -12,6 +12,9 @@
  * limitations under the License.
  */
 
+#include <iostream>
+#include <thread>
+
 #include <asiodnp3/DNP3Manager.h>
 #include <asiodnp3/PrintingSOEHandler.h>
 #include <asiodnp3/ConsoleLogger.h>
@@ -19,11 +22,14 @@
 #include <opendnp3/outstation/Database.h>
 #include <opendnp3/LogLevels.h>
 
-#include <iostream>
-#include <thread>
+#include <boost/program_options.hpp>
+
+#include <yaml-cpp/yaml.h>
 
 #include "AsyncCommandHandler.cpp"
 #include "OutstationJSONTCPServer.cpp"
+
+namespace po = boost::program_options;
 
 using namespace opendnp3;
 using namespace openpal;
@@ -45,53 +51,115 @@ void signal_handler(int signal_number) {
  */
 class OutstationApp {
 public:
-	void start() {
+	/**
+	 * Validates that config contains needed elements for basic configuration.
+	 * Does not validate correctness of config elements.
+	 */
+	static bool validConfig(YAML::Node& config) {
+		bool valid = config["master"] && config["master"]["id"] && config["master"]["host"] && config["master"]["port"] && config["outstations"]
+				&& config["outstations"][0] && config["outstations"][0]["id"] && config["outstations"][0]["localAddr"]
+				&& config["outstations"][0]["remoteAddr"];
+		if (!valid)
+			std::cerr << "Invalid YAML config, check master (id, host, port) and ou tstations (id)" << std::endl;
+		return valid;
+	}
+
+	void start(YAML::Node& config) {
 		DNP3Manager manager(1);
 		manager.AddLogSubscriber(&ConsoleLogger::Instance());
 
-		IChannel* pChannel = manager.AddTCPServer("server", levels::NORMAL, ChannelRetry::Default(), "0.0.0.0", 20000);
+		IChannel* pChannel = configureManagerMaster(config, manager);
 		pChannel->AddStateListener([](ChannelState state)
 		{
 			std::cout << "channel state: " << ChannelStateToString(state) << std::endl;
 		});
 
-		OutstationStackConfig stackConfig;
-		stackConfig.dbTemplate = DatabaseTemplate::AllTypes(5);
-		stackConfig.outstation.eventBufferConfig = EventBufferConfig::AllTypes(5);
-		stackConfig.link.LocalAddr = 10;
-		stackConfig.link.RemoteAddr = 1;
-
 		AsyncCommandHandler handler;
-		IOutstation* pOutstation = pChannel->AddOutstation("outstation", handler, DefaultOutstationApplication::Instance(), stackConfig);
+		/** TODO for each outstation, configure **/
+		for (std::size_t i = 0; i < config["outstations"].size(); i++) {
+			YAML::Node outstationConfig = config["outstations"][i];
+			IOutstation* pOutstation = configureOutstation(outstationConfig, pChannel, handler);
 
-		// Configure and start outstation
-		ConfigureDatabase(pOutstation->GetConfigView());
-		pOutstation->Enable();
+			/** TODO Enable all at end of loop, keep separate list for OJTS, add multi-outstation handling **/
+			pOutstation->Enable();
+			OutstationJSONTCPServer s(io_service_, 3384, pOutstation, handler);
+			io_service_.run();
+		}
 
-		OutstationJSONTCPServer s(io_service_, 3384, pOutstation, handler);
-		io_service_.run();
 	}
 
 	void stop() {
-		// TODO shutdown correctly
+		// TODO shutdown correctly, consider DNP3Manager.shutdown()
 		io_service_.stop();
 	}
 private:
-	void ConfigureDatabase(DatabaseConfigView view) {
+	IChannel* configureManagerMaster(YAML::Node config, DNP3Manager& manager) {
+		return manager.AddTCPServer("server", levels::NORMAL, ChannelRetry::Default(), "0.0.0.0", 20000);
+	}
+
+	IOutstation* configureOutstation(YAML::Node& outConf, IChannel* pChannel, AsyncCommandHandler& handler) {
+		OutstationStackConfig stackConfig;
+		stackConfig.link.LocalAddr = outConf["localAddr"].as<int>();
+		stackConfig.link.RemoteAddr = outConf["remoteAddr"].as<int>();
+
+		if (outConf["measDB"]) {
+			int binaries = outConf["measDB"]["binaries"].size();
+			int doubleBinaries = outConf["measDB"]["doubleBinaries"].size();
+			int analogs = outConf["measDB"]["analogs"].size();
+			int counters = outConf["measDB"]["counters"].size();
+			int frozenCounters = outConf["measDB"]["frozenCounters"].size();
+			int binaryOutputStatii = outConf["measDB"]["binaryOutputStatii"].size();
+			int analogOutputStatii = outConf["measDB"]["analogOutputStatii"].size();
+			int timeAndIntervals = outConf["measDB"]["timeAndIntervals"].size();
+
+			stackConfig.dbTemplate = DatabaseTemplate(binaries, doubleBinaries, analogs, counters, frozenCounters, binaryOutputStatii, analogOutputStatii,
+					timeAndIntervals);
+			stackConfig.outstation.eventBufferConfig = EventBufferConfig(binaries, doubleBinaries, analogs, counters, frozenCounters, binaryOutputStatii,
+					analogOutputStatii, timeAndIntervals);
+		} else {
+			/** using default demo configuration **/
+			stackConfig.dbTemplate = DatabaseTemplate::AllTypes(5);
+			stackConfig.outstation.eventBufferConfig = EventBufferConfig::AllTypes(5);
+		}
+
+		IOutstation* pOutstation = pChannel->AddOutstation(((std::string) outConf["id"].as<std::string>()).c_str(), handler,
+				DefaultOutstationApplication::Instance(), stackConfig);
+
+		DatabaseConfigView view = pOutstation->GetConfigView();
 		// TODO read from config file
 		view.analogs[0].variation = StaticAnalogVariation::Group30Var5;
 		view.analogs[0].metadata.clazz = PointClass::Class2;
 		view.analogs[0].metadata.variation = EventAnalogVariation::Group32Var7;
+
+		return pOutstation;
 	}
 
 	boost::asio::io_service io_service_;
 };
 
 int main(int argc, char* argv[]) {
+	std::string confFile;/** "cpp/test/outstation/demo.yml" **/
+
 	std::signal(SIGINT, signal_handler);
 
+	po::options_description desc("Options");
+	desc.add_options()("conf", po::value<std::string>(&confFile)->required(), "yaml configuration file")("help", "show help");
+
+	po::variables_map vm;
+	po::store(po::parse_command_line(argc, argv, desc), vm);
+	po::notify(vm);
+
+	if (vm.count("help")) {
+		std::cerr << desc << std::endl;
+		return 1;
+	}
+
+	YAML::Node config = YAML::LoadFile(confFile);
+	if (!OutstationApp::validConfig(config))
+		return 1;
+
 	OutstationApp app;
-	std::thread t([&app] {app.start();});
+	std::thread t([&app, &config] {app.start(config);});
 	t.detach();
 
 	std::unique_lock<std::mutex> lock(g_signal_mutex);
